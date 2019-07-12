25ddff333b9d72bab780a0ab6e7f9a69fa2dcd1d
Alexander Cai <alexandercai@outlook.com>
2019-07-12 08:53:51 -0600

    successfully implemented passport-discord

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const { Strategy } = require('passport-discord');
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
+
+const scopes = ['identify', 'email', /* 'connections', (it is currently broken) */ 'guilds', 'guilds.join'];
+
+passport.use(new Strategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: `http://localhost:${PORT}/api/discord/callback`,
+  scope: scopes
+}, (accessToken, refreshToken, profile, done) => {
+  process.nextTick(() => done(null, profile));
+}));
+
 express()
+  .use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
   .use(express.static(path.join(__dirname, 'public')))
-  .use('/api/discord', require('./api/discord'))
+  // .use('/api/discord', require('./api/discord'))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .use('/api/discord/login', passport.authenticate('discord', { scope: scopes }))
+  .get('/api/discord/callback', passport.authenticate('discord', {
+    failureRedirect: '/',
+    successRedirect: '/loggedin'
+  }))
+  .use('/api/discord/logout', (req, res) => {
+    req.logout();
+    res.redirect('/')
+  })
+  .get('/loggedin', (req, res) => {
+    res.status(200).send('You have been logged in');
+  })
   .use((err, req, res, next) => {
     switch (err.message) {
       case 'NoCodeProvided':
