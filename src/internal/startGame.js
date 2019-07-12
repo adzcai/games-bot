36f124faae7d36165ac0608c1604c6afe73d8ca8
Alexander Cai <alexandercai@outlook.com>
2019-07-11 23:44:37 -0600

    cleaned up startGame.js

--- src/internal/startGame.js
+++ src/internal/startGame.js
@@ -1,32 +1,30 @@
-
-
-let commands;
+const commands = require('./getCommands.js');
 
 /*
  * This function simply starts the game. It is called when a player types the prefix followed by
  * the name of the game.
  */
 
-module.exports = async function (message, args, gameClass) {
+module.exports = async function startGame(message, args, GameClass) {
   const server = global.servers[message.guild.id];
 
   // If the player is already in a game, see if they call any arguments
-  const playerGameID = server.players[message.author.id][gameClass.type];
+  const playerGameID = server.players[message.author.id][GameClass.type];
   let argsPassed = false;
   if (playerGameID) {
     const playerGame = server.games[playerGameID];
-    commands = (require('./getCommands.js'))();
-    for (let i = 0; i < args.length; i++) {
-      if (commands[playerGame.command].options.hasOwnProperty(args[i])) {
-        args[i].action.call(playerGame, message, args);
+    args.forEach((arg) => {
+      if (arg in commands[playerGame.command].options) {
+        arg.action.call(playerGame, message, args);
         argsPassed = true;
       }
-    }
+    });
     if (argsPassed) return;
-    return playerGame.channel.send(`You are already in a game! Type ${process.env.DEFAULT_PREFIX}${playerGame.type} view to resend the message.`).catch(global.logger.error);
+    playerGame.channel.send(`You are already in a game! Type ${process.env.DEFAULT_PREFIX}${playerGame.type} view to resend the message.`).catch(global.logger.error);
+    return;
   }
 
   const id = Math.random().toString(36).substr(2, 9);
-  server.games[id] = new gameClass(id, message.channel);
+  server.games[id] = new GameClass(id, message.channel);
   server.games[id].init(message, args);
 };
