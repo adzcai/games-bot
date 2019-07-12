e352d3c4167cae600c7f6c29826f382fdaba45da
Alexander Cai <alexandercai@outlook.com>
2019-07-12 12:24:05 -0600

    minor change in exception handling

--- bot.js
+++ bot.js
@@ -86,14 +86,21 @@ const handle = setInterval(pruneEndedGames, 5 * 60 * 1000);
  * This exit handler simply makes sure the program terminates gracefully when
  * it is killed, nodemon restarts, or an error occurs.
  */
-const exitHandler = function (exitCode) {
+function exitHandler(exitCode) {
   clearInterval(handle);
   logger.info('Interval cleared');
-  if (exitCode !== undefined) logger.info(exitCode);
+  if (exitCode) logger.info(`Exit code: ${exitCode}`);
   process.exit();
-};
+}
 
 process.on('SIGINT', exitHandler);
+process.on('SIGTERM', exitHandler);
 process.on('SIGUSR1', exitHandler);
 process.on('SIGUSR2', exitHandler);
-process.on('uncaughtException', exitHandler);
+/**
+ * We set up an uncaught exception capture callback so that the bot keeps running even when an
+ * error occurs
+ */
+process.setUncaughtExceptionCaptureCallback((err) => {
+  logger.error(`An error occurred: ${err}`);
+});
