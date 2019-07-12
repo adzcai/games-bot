6bf7e3d7134bd28e6d9173203d35edf4b9b3a7b8
Alexander Cai <alexandercai@outlook.com>
2019-07-12 09:31:14 -0600

    improved views and paths

--- src/commands/pi.js
+++ src/commands/pi.js
@@ -9,7 +9,7 @@ module.exports = {
     },
   },
   run: (message, args) => {
-    fs.readFile('../../res/pidigits.txt', (err, data) => {
+    fs.readFile('../../public/assets/pidigits.txt', (err, data) => {
       if (err) throw err;
       const numdigs = parseInt(args[0]) || 5;
       message.channel.send(data.substring(0, numdigs)).catch(logger.error);
